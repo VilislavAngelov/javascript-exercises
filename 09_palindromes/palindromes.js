93863b704296dec4fb485c197327c8bc672633f2
Vilislav <vilislavangelov@gmail.com>
2024-10-28 19:07:35 +0200

    Did the 09 palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let pattern = /[a-z0-9]/gi;
+    let cleanStr = str.toLowerCase().match(pattern).join("");
+    let reverse = cleanStr.split("").reverse().join("");
+  
+    if(cleanStr === reverse) {
+      return true;
+    }
+      return false;
 };
 
 // Do not edit below this line
